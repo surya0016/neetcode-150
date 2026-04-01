3f09f20d7aa8678cdc657ab8474dd290daab5160
Zoot009 <zootdigitalseo@gmail.com>
2026-04-01 20:25:47 +0530

    solved valid-anagram

--- src/strings/valid-anagram.ts
+++ src/strings/valid-anagram.ts
@@ -1,17 +1,47 @@
-// const isAnagram = (s:string, t:string){
-//   if (s.length !== t.length){
-//     return false
-//   }
-//
-//   const mapS = new Map()
-//   const mapT = new Map()
-//
-//
-//
-//
-// }
+const isAnagram = (s:string, t:string) => {
+  if (s.length !== t.length){
+    return false
+  }
 
-// const result = isAnagram('racecar', 'carrace')
+  const mapS = new Map()
+  const mapT = new Map()
 
-const result = 'surya'
-console.log(result.length)
+  for (let i = 0; i < s.length; i++) {
+    const skey = s[i]
+    const tkey = t[i] 
+
+  //   if (mapS.has(skey)){
+  //     mapS.set(skey, mapS.get(skey) + 1);
+  //   }else{
+  //     mapS.set(skey, 1)
+  //   }
+  //
+  //   if (mapT.has(tkey)){
+  //     mapT.set(tkey, mapT.get(tkey) + 1);
+  //   }else{
+  //     mapT.set(tkey, 1)
+  //   }
+
+    mapS.set(skey, (mapS.get(skey) ?? 0) +1)
+    mapT.set(tkey, (mapT.get(tkey) ?? 0) +1)
+  }
+
+  if(mapS.size !== mapT.size){
+    return false;
+  }
+
+  for(const[key, value] of mapS){
+    if(!mapT.has(key)){
+      return false;
+    }
+
+    if(mapT.get(key) !== value){
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const result = isAnagram('racecar', 'carrace')
+console.log(result)
